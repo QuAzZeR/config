8987e7c9f5fadb5ed53dc8a3ce59585fe6f0d754
padungkiat-tw <padungkiat.tamasee@thoughtworks.com>
2023-07-12 00:39:24 +0700

    Remove unused file

--- neovim/plugin/lspconfig.lua
+++ neovim/plugin/lspconfig.lua
@@ -106,6 +106,32 @@ nvim_lsp.lua_ls.setup {
   },
 }
 
+nvim_lsp.lua_ls.setup {
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    enable_format_on_save(client, bufnr)
+  end,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false
+      },
+    },
+  },
+}
+
+nvim_lsp.tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
 
 nvim_lsp.cssls.setup {
   on_attach = on_attach,
