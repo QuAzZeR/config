fc62024f9b7abf0c5f0de3b601f79eb952947edd
quazzer <natsamiss@gmail.com>
2023-12-15 23:05:11 +0700

    :sparkles: Update configuration for neovim and remove python from lazyvim

--- lazyvim/lua/plugins/lang.lua
+++ lazyvim/lua/plugins/lang.lua
@@ -3,9 +3,43 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {},
+        pyright = {
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+          },
+        },
       },
       setup = {},
     },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
   },
 }
--- neovim/lua/quazzer/remap.lua
+++ neovim/lua/quazzer/remap.lua
@@ -1,53 +1,55 @@
 local keymap = vim.keymap
-
+local opts = { noremap = true, silent = true }
 
 -- Map Leader
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 -- Ignore Arrow Key
-keymap.set('', '<UP>', '<NOP>')
-keymap.set('', '<DOWN>', '<NOP>')
-keymap.set('', '<LEFT>', '<NOP>')
-keymap.set('', '<RIGHT>', '<NOP>')
-keymap.set('i', '<UP>', '<NOP>')
-keymap.set('i', '<DOWN>', '<NOP>')
-keymap.set('i', '<LEFT>', '<NOP>')
-keymap.set('i', '<RIGHT>', '<NOP>')
+keymap.set("", "<UP>", "<NOP>")
+keymap.set("", "<DOWN>", "<NOP>")
+keymap.set("", "<LEFT>", "<NOP>")
+keymap.set("", "<RIGHT>", "<NOP>")
+keymap.set("i", "<UP>", "<NOP>")
+keymap.set("i", "<DOWN>", "<NOP>")
+keymap.set("i", "<LEFT>", "<NOP>")
+keymap.set("i", "<RIGHT>", "<NOP>")
 
 -- new Tab
-keymap.set('n', 'tn', ':tabnew<Return>')
+keymap.set("n", "tn", ":tabnew<Return>", opts)
 
 -- Tab Movement
-keymap.set('n', '<leader>0', '10gt')
-keymap.set('n', '<leader>1', '1gt')
-keymap.set('n', '<leader>2', '2gt')
-keymap.set('n', '<leader>3', '3gt')
-keymap.set('n', '<leader>4', '4gt')
-keymap.set('n', '<leader>5', '6gt')
-keymap.set('n', '<leader>6', '6gt')
-keymap.set('n', '<leader>7', '7gt')
-keymap.set('n', '<leader>8', '8gt')
-keymap.set('n', '<leader>9', '9gt')
+keymap.set("n", "<leader>0", "10gt")
+keymap.set("n", "<leader>1", "1gt")
+keymap.set("n", "<leader>2", "2gt")
+keymap.set("n", "<leader>3", "3gt")
+keymap.set("n", "<leader>4", "4gt")
+keymap.set("n", "<leader>5", "6gt")
+keymap.set("n", "<leader>6", "6gt")
+keymap.set("n", "<leader>7", "7gt")
+keymap.set("n", "<leader>8", "8gt")
+keymap.set("n", "<leader>9", "9gt")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split window
-keymap.set('n', '<leader>s', ':sp<CR>')
-keymap.set('n', '<leader>v', ':vsp<CR>')
+keymap.set("n", "<leader>s", ":sp<CR>", opts)
+keymap.set("n", "<leader>v", ":vsp<CR>", opts)
 
 -- Move window
-keymap.set('n', '<leader>j', '<C-W>j')
-keymap.set('n', '<leader>h', '<C-W>h')
-keymap.set('n', '<leader>k', '<C-W>k')
-keymap.set('n', '<leader>l', '<C-W>l')
-keymap.set('n', '<leader>x', '<C-W>q')
+keymap.set("n", "<leader>j", "<C-W>j")
+keymap.set("n", "<leader>h", "<C-W>h")
+keymap.set("n", "<leader>k", "<C-W>k")
+keymap.set("n", "<leader>l", "<C-W>l")
+keymap.set("n", "<leader>x", "<C-W>q")
 
 -- Resize window
-keymap.set('n', '<leader><left>', '<C-W><')
-keymap.set('n', '<leader><right>', '<C-W>>')
-keymap.set('n', '<leader><up>', '<C-W>+')
-keymap.set('n', '<leader><down>', '<C-W>-')
+keymap.set("n", "<leader><left>", "<C-W><")
+keymap.set("n", "<leader><right>", "<C-W>>")
+keymap.set("n", "<leader><up>", "<C-W>+")
+keymap.set("n", "<leader><down>", "<C-W>-")
 
 -- Move Buffer
-keymap.set('n', '<C-S-j>', ':bp<CR>')
-keymap.set('n', 'q', ':bd<CR>')
-keymap.set('n', '<C-S-k>', ':bn<CR>')
+keymap.set("n", "<C-S-j>", ":bp<Return>", opts)
+keymap.set("n", "q", ":bd<CR>", opts)
+keymap.set("n", "<C-S-k>", ":bn<Return>", opts)
 
-keymap.set('n', '<C-i>', '<C-I>')
+keymap.set("n", "<C-i>", "<C-I>")
