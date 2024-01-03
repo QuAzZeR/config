a5fde750e8514c9087b1cd6ca60b9a02558fda68
quazzer <natsamiss@gmail.com>
2024-01-03 23:09:31 +0700

    :sparkles: start lang configuration again

--- lazyvim/lua/plugins/lang.lua
+++ lazyvim/lua/plugins/lang.lua
@@ -81,4 +81,24 @@ return {
       },
     },
   },
+
+  -- Markdown
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
 }
