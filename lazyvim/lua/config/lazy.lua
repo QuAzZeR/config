89afcf583e2a81685ceddacacb0ee7d41b00a089
quazzer <natsamiss@gmail.com>
2024-03-29 23:52:24 +0700

    :wrench: update theme to tokyonight-storm

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "tokyonight",
+        colorscheme = "tokyonight-storm",
         news = {
           lazyvim = true,
           neovim = true,
